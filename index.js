624e5509206c6acf19b847d0153b3470dc9f808c
Simon Andrei <simon.andrei879@gmail.com>
2020-03-26 09:08:08 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+// import App from 'react-native-ble-manager/example/App';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
